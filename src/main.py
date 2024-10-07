958d8fbc866abcfba48dc68aa3d7d346dfeb7a08
Quentin <quentin.bollore@epitech.eu>
2024-10-07 23:15:09 +0200

    feat(main): fix error

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ import arg_handler as argparse
 if __name__ == "__main__":
     args = argparse.parse_arguments()
 
-    message = sys.stdin.read()
+    if args['MODE'] == '-g':
+        message = ''
+    else:
+        message = sys.stdin.read()
 
     result = argparse.choose_crypto_system(args, message.strip())
 
